768060b13e35d825f898d8ec5033c47bc74a92bd
NCTUyoung <jimmyyoung1995@gmail.com>
2019-05-07 22:23:34 +0800

    finish color

--- graph.js
+++ graph.js
@@ -1,4 +1,76 @@
-var data = []
+const dims = {
+  height:500,
+  width:1100
+};
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width',dims.width + 100)
+    .attr('height',dims.height+100);
+
+const graph = svg.append('g')
+    .attr('transform',`translate(50,50)`);
+
+//data strat
+const stratify = d3.stratify()
+    .id(d=>d.name)
+    .parentId(d=>d.parent);
+const tree = d3.tree()
+    .size([dims.width,dims.height]);
+
+const colour = d3.scaleOrdinal(['#f4511e','#e91e63','#e53935','#9c27b0']);
+//update function
+const update = (data) => {
+    //remove all node
+    graph.selectAll('.node').remove();
+    graph.selectAll('.link').remove();
+
+
+    colour.domain(data.map(item =>item.department));
+    const rootNode = stratify(data);
+    const treeData = tree(rootNode);
+    // console.log(treeData)
+    //get node selection and join data
+    const nodes = graph.selectAll('.node')
+        .data(treeData.descendants());
+    const links = graph.selectAll('.link')
+        .data(treeData.links());
+    //enter new links
+    links.enter()
+        .append('path')
+        .transition().duration(300)
+        .attr('class','link')
+        .attr('fill','none')
+        .attr('stroke','#aaa')
+        .attr('stroke-width',2)
+        .attr('d',d3.linkVertical()
+            .x(d=>d.x)
+            .y(d=>d.y)
+        );
+    //create enter node groups
+    const enterNodes = nodes.enter()
+        .append('g')
+            .attr('class','node')
+        .attr('transform',d=>`translate(${d.x},${d.y})`);
+    //append rect to enter nodes
+    enterNodes.append('rect')
+        .attr('fill',d=>colour(d.data.department))
+        .attr('stroke','#555')
+        .attr('stroke-width',2)
+        .attr('height',50)
+        .attr('width',d=>d.data.name.length* 20)
+        .attr('transform',d=>{
+            var x = d.data.name.length* 10;
+            return `translate(${-x},-25)`
+        })
+    enterNodes.append('text')
+        .attr('text-anchor','middle')
+        .attr('fill','white')
+        .text(d=>d.data.name)
+
+
+};
+
+var data = [];
 
 db.collection('employees').onSnapshot(res=>{
     res.docChanges().forEach(change=>{
@@ -18,7 +90,8 @@ db.collection('employees').onSnapshot(res=>{
             default:
                 break;
         }
-    })
+    });
+
+    update(data);
 });
 
-console.log(data);
